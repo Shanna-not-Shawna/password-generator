facf551c4cdd142fced967fa740c996f63e5d8b6
Shanna-not-Shawna <144177634+Shanna-not-Shawna@users.noreply.github.com>
2023-09-21 20:01:55 -0500

    Added functions to javascript

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var special = ["?", "!", "@", "#", "$", "%", "&", "*"]
+var numArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+
+var passwordLength = 0;
+var useSpecial = false;
+var useNumbers = false;
+var useUpper = false;
+var useLower = false;
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -10,13 +22,7 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
-var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var special = ["?", "!", "@", "#", "$", "%", "&", "*"]
-var numArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 
 
 // input fields have .value on click 
@@ -25,34 +31,83 @@ var numArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 
 
 function generatePassword() {
+  var validCriteria = promptUser();
+
+  if (!validCriteria) {
+    return "Please Try Again!"
+  }
+
+  var selectedCharacters = [];
+  var password = "";
+
+  if(useLower){
+    //add chars to selected
+    selectedCharacters.push(...lower);
+
+    //make sure password has at least one of this character
+    password += getRandomCharacter(lower)
+  }
+
+
+  
+
+  while(password.length < passwordLength){
+    //add another character
+    password += getRandomCharacter(selectedCharacters)
+  }
+
   // acceptance criteria
-  return "mySecretPassword"
+  return password
 }
 
 
 
 // Math.floor(Math.random) to generate random character
-
-password += Math.floor(Math.random() * upper.length)
-password += Math.floor(Math.random() * lower.length)
-password += Math.floor(Math.random() * special.length)
-password += Math.floor(Math.random() * numArr.length)
-
+function getRandomCharacter(arr) {
+  var randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
+}
 // loop generates 1 character at a time and stops when the number of characters entered is reached
 
-// add condition to be between 8 and 129 characters
-window.prompt("How many characters do you want your password to be?");
+function promptUser() {// add condition to be between 8 and 129 characters
+  passwordLength = window.prompt("How many characters do you want your password to be?");
 
-// if true, include special in Math.random calculation
-window.confirm("Would you like special characters included?");
+  if (isNaN(passwordLength)) {
+    window.alert("Please enter a valid Number!");
+    return false;
+  }
 
-// if true, include numbers in Math.random calculation
-window.confirm("Would you like numbers included?");
+  passwordLength = parseInt(passwordLength);
 
-// if true, upper numbers in Math.random calculation
-window.confirm("Would you like upppercase letters included?");
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Please enter a number between 8 and 128.");
+    return false;
+  }
 
-// if true, include lower in Math.random calculation
-window.confirm("would you like lowercase letters included?");
+  // if true, include special in Math.random calculation
+  useSpecial = window.confirm("Would you like special characters included?");
+
+  // if true, include numbers in Math.random calculation
+  useNumbers = window.confirm("Would you like numbers included?");
+
+  // if true, upper numbers in Math.random calculation
+  useUpper = window.confirm("Would you like uppercase letters included?");
+
+  // if true, include lower in Math.random calculation
+  useLower = window.confirm("would you like lowercase letters included?");
+
+  if (!useSpecial && !useNumbers && !useUpper && !useLower) {
+    window.alert("Please select at least one character type");
+    return false;
+  }
+
+  return true;
+
+}
+
+// ***if all are false, need an alert saying you must pick one
 
 
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
