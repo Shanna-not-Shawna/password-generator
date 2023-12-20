4fcdad5c412d0d44fbf104c2f3d32c41e013d7f8
Shanna-not-Shawna <144177634+Shanna-not-Shawna@users.noreply.github.com>
2023-12-19 19:54:33 -0600

    modified generatePassword function

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,3 @@
-
-import 'bootstrap';
-
 var upperWords = ["Aqua", "Azure", "Beige", "Black", "Blue", "Brown", "Burgundy", "Coral", "Crimson", "Cyan", "Emerald", "Fuchsia", "Gold", "Green", "Indigo", "Ivory", "Lavender", "Lilac", "Magenta", "Maroon", "Navy", "Olive", "Orange", "Pink", "Purple", "Red", "Silver", "Teal", "Violet", "White", "Yellow", "Amazing", "Beautiful", "Clever", "Delightful", "Elegant", "Fantastic", "Gorgeous", "Happy", "Incredible", "Joyful", "Kind", "Lovely", "Nice", "Outstanding", "Peaceful", "Quiet", "Radiant", "Serene", "Tranquil", "Unique", "Valuable", "Wonderful"]
 var lowerWords = ["appliance", "apple", "pineapple", "bed", "blanket", "book", "brush", "cabinet", "chair", "clock", "computer", "cup", "curtain", "desk", "door", "drawer", "faucet", "floor", "fork", "glass", "hanger", "hat", "key", "knife", "lamp", "light", "mirror", "mop", "nail", "needle", "paper", "pen", "pillow", "plate", "pot", "rug", "scissors", "shelf", "shirt", "shoe", "sink", "soap", "sock", "spoon", "stove", "table", "towel", "vase", "wall", "window"]
 var special = ["?", "!", "@", "#", "$", "%", "&", "*"]
@@ -26,13 +23,11 @@ function writePassword() {
 }
 
 function generatePassword() {
-  var validCriteria = promptUser();
-
-  // if invalid input from user
-  if (!validCriteria) {
-    return "Please Try Again!"
-  }
-
+  var passwordLength = document.querySelector("#length").value;
+  var useSpecial = document.querySelector("#special").checked;
+  var useNumbers = document.querySelector("#numbers").checked;
+  var useUpper = document.querySelector("#upper").checked;
+  var useLower = true;
   var selectedCharacters = [];
   var password = "";
 
@@ -86,50 +81,50 @@ function getRandomCharacter(arr) {
 }
 // loop generates 1 character at a time and stops when the number of characters entered is reached
 
-function promptUser() {
-  passwordLength = window.prompt("How many characters do you want your password to be?");
+// function promptUser() {
+//   passwordLength = window.prompt("How many characters do you want your password to be?");
 
-  // check for empty string
-  if (!passwordLength) {
-    window.alert("Please enter a valid number!");
-    return false;
-  }
+//   // check for empty string
+//   if (!passwordLength) {
+//     window.alert("Please enter a valid number!");
+//     return false;
+//   }
 
-  // check for numerical input
-  if (isNaN(passwordLength)) {
-    window.alert("Please enter a valid Number!");
-    return false;
-  }
+//   // check for numerical input
+//   if (isNaN(passwordLength)) {
+//     window.alert("Please enter a valid Number!");
+//     return false;
+//   }
 
-  // change string to number
-  passwordLength = parseInt(passwordLength);
+//   // change string to number
+//   passwordLength = parseInt(passwordLength);
 
-  // check if number is within range
-  if (passwordLength < 8 || passwordLength > 128) {
-    window.alert("Please enter a number between 8 and 128.");
-    return false;
-  }
+//   // check if number is within range
+//   if (passwordLength < 8 || passwordLength > 128) {
+//     window.alert("Please enter a number between 8 and 128.");
+//     return false;
+//   }
 
-  // if true, push array contents into selectedCharacters
-  useSpecial = window.confirm("Would you like special characters included?");
+//   // if true, push array contents into selectedCharacters
+//   useSpecial = window.confirm("Would you like special characters included?");
 
-  // if true, push array contents into selectedCharacters
-  useNumbers = window.confirm("Would you like numbers included?");
+//   // if true, push array contents into selectedCharacters
+//   useNumbers = window.confirm("Would you like numbers included?");
 
-  // if true, push array contents into selectedCharacters
-  useUpper = window.confirm("Would you like uppercase letters included?");
+//   // if true, push array contents into selectedCharacters
+//   useUpper = window.confirm("Would you like uppercase letters included?");
 
-  // if true, push array contents into selectedCharacters
-  useLower = window.confirm("Would you like lowercase letters included?");
+//   // if true, push array contents into selectedCharacters
+//   useLower = window.confirm("Would you like lowercase letters included?");
 
-  if (!useSpecial && !useNumbers && !useUpper && !useLower) {
-    window.alert("Please select at least one character type");
-    return false;
-  }
+//   if (!useSpecial && !useNumbers && !useUpper && !useLower) {
+//     window.alert("Please select at least one character type");
+//     return false;
+//   }
 
-  return true;
+//   return true;
 
-}
+// }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
