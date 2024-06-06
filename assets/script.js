a8b94ca711bd15151ef9224a17ede0155ffcb83b
Shanna-not-Shawna <144177634+Shanna-not-Shawna@users.noreply.github.com>
2024-06-05 23:58:32 -0500

    uncommented out code for troubleshooting

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,12 @@
-// const upperWords = ["Adventurous", "Amazing", "Aqua", "Azure", "Beautiful", "Beige", "Black", "Blue", "Blissful", "Brown", "Bougie", "Burgundy", "Charming", "Clever", "Coral", "Crimson", "Cyan", "Daring", "Delightful", "Dazzling", "Elegant", "Energetic", "Ethereal", "Fantastic", "Fascinating", "Friendly", "Gorgeous", "Graceful", "Green", "Harmonious", "Happy", "Incredible", "Indigo", "Intelligent", "Inventive", "Ivory", "Jubilant", "Joyful", "Kind", "Kinetic", "Lavender", "Lilac", "Lovely", "Lively", "Magnificent", "Magenta", "Maroon", "Noble", "Nice", "Optimistic", "Outstanding", "Peaceful", "Playful", "Quick", "Radiant", "Red", "Silver", "Sincere", "Talented", "Teal", "Tranquil", "Unique", "Understanding", "Valuable", "Vibrant", "White", "Wonderful", "Witty", "Youthful", "Yellow", "Zealous"]
-// const lowerWords = ["air", "airplane", "appliance", "apple", "at", "ax", "basement", "bathtub", "bed", "book", "brush", "cabinet", "car", "chair", "city", "classroom", "clock", "coffee", "computer", "cup", "curtain", "desk", "dog", "dolphin", "door", "drawer", "do", "earthquake", "elevator", "elephant", "faucet", "family", "five", "floor", "flower", "fork", "food", "four", "friend", "glass", "go", "hat", "heart", "house", "if", "in", "is", "it", "key", "lamp", "lemon", "light", "lime", "me", "mirror", "movie", "music", "my", "no", "ocean", "ok", "one", "paper", "pen", "pear", "phone", "pineapple", "plate", "pot", "river", "rug", "school", "scissors", "shelf", "shirt", "shoe", "sink", "soap", "sock", "six", "spoon", "suitcase", "sun", "table", "telephone", "three", "time", "to", "toothbrush", "train", "tree", "two", "up", "us", "vase", "window"]
+const upperWords = ["Adventurous", "Amazing", "Aqua", "Azure", "Beautiful", "Beige", "Black", "Blue", "Blissful", "Brown", "Bougie", "Burgundy", "Charming", "Clever", "Coral", "Crimson", "Cyan", "Daring", "Delightful", "Dazzling", "Elegant", "Energetic", "Ethereal", "Fantastic", "Fascinating", "Friendly", "Gorgeous", "Graceful", "Green", "Harmonious", "Happy", "Incredible", "Indigo", "Intelligent", "Inventive", "Ivory", "Jubilant", "Joyful", "Kind", "Kinetic", "Lavender", "Lilac", "Lovely", "Lively", "Magnificent", "Magenta", "Maroon", "Noble", "Nice", "Optimistic", "Outstanding", "Peaceful", "Playful", "Quick", "Radiant", "Red", "Silver", "Sincere", "Talented", "Teal", "Tranquil", "Unique", "Understanding", "Valuable", "Vibrant", "White", "Wonderful", "Witty", "Youthful", "Yellow", "Zealous"]
+const lowerWords = ["air", "airplane", "appliance", "apple", "at", "ax", "basement", "bathtub", "bed", "book", "brush", "cabinet", "car", "chair", "city", "classroom", "clock", "coffee", "computer", "cup", "curtain", "desk", "dog", "dolphin", "door", "drawer", "do", "earthquake", "elevator", "elephant", "faucet", "family", "five", "floor", "flower", "fork", "food", "four", "friend", "glass", "go", "hat", "heart", "house", "if", "in", "is", "it", "key", "lamp", "lemon", "light", "lime", "me", "mirror", "movie", "music", "my", "no", "ocean", "ok", "one", "paper", "pen", "pear", "phone", "pineapple", "plate", "pot", "river", "rug", "school", "scissors", "shelf", "shirt", "shoe", "sink", "soap", "sock", "six", "spoon", "suitcase", "sun", "table", "telephone", "three", "time", "to", "toothbrush", "train", "tree", "two", "up", "us", "vase", "window"]
 const special = ["?", "!", "@", "#", "$", "%", "&", "*"]
 const numArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 const lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 const upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 const passwordLength = 0;
-// const useWords = false;
+const useWords = false;
 const useSpecial = false;
 const useNumbers = false;
 const useUpper = false;
@@ -17,14 +17,14 @@ const generateBtn = document.querySelector("#generate");
 const resetBtn = document.querySelector("#resetBtn");
 const container = document.querySelector("#card");
 
-// const wordsCheckbox = document.querySelector("#words");
+const wordsCheckbox = document.querySelector("#words");
 const upperCheckbox = document.querySelector("#upper");
 const specialCheckbox = document.querySelector("#special");
 const numbersCheckbox = document.querySelector("#numbers");
 
-// wordsCheckbox.addEventListener("change", () => {
-//   console.log(`'Use Words' checkbox checked: ${wordsCheckbox.checked}`);
-// });
+wordsCheckbox.addEventListener("change", () => {
+  console.log(`'Use Words' checkbox checked: ${wordsCheckbox.checked}`);
+});
 
 upperCheckbox.addEventListener("change", () => {
   console.log(`Use Upper checkbox checked: ${upperCheckbox.checked}`);
@@ -49,74 +49,74 @@ const generatePassword = event => {
     event.preventDefault();
   }
 
-  // const useExactLength = document.querySelector("#useExact").checked;
-  // const useWords = document.querySelector("#words").checked;
+  const useExactLength = document.querySelector("#useExact").checked;
+  const useWords = document.querySelector("#words").checked;
   const useSpecial = document.querySelector("#special").checked;
   const useNumbers = document.querySelector("#numbers").checked;
   const useUpper = document.querySelector("#upper").checked;
-  // const passwordLengthInput = document.querySelector("#length");
-  // const passwordLength = parseInt(passwordLengthInput.value, 10);
+  const passwordLengthInput = document.querySelector("#length");
+  const passwordLength = parseInt(passwordLengthInput.value, 10);
 
-  // const selectedCharacters = [];
+  const selectedCharacters = [];
   let password = "";
 
-  // const getRandomCharacter = arr => {
-  //   const randomIndex = Math.floor(Math.random() * arr.length);
-  //   return arr[randomIndex];
-  // }
+  const getRandomCharacter = arr => {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+  }
 
-  // const addCharacters = (arr, isWord) => {
-  //   if (!useWords || (useWords && isWord)) {
-  //     while (password.length < passwordLength && arr.length > 0) {
-  //       console.log(`Entering loop. Current password:`, password);
-  //       let randomChar = getRandomCharacter(arr);
+  const addCharacters = (arr, isWord) => {
+    if (!useWords || (useWords && isWord)) {
+      while (password.length < passwordLength && arr.length > 0) {
+        console.log(`Entering loop. Current password:`, password);
+        let randomChar = getRandomCharacter(arr);
   
-  //       console.log(`randomChar.length:`, randomChar.length);
-  //       console.log(`password.length:`, password.length);
-  //       console.log('passwordLength', passwordLength);
+        console.log(`randomChar.length:`, randomChar.length);
+        console.log(`password.length:`, password.length);
+        console.log('passwordLength', passwordLength);
   
-  //       if (randomChar.length + password.length <= passwordLength) {
-  //         console.log(`Adding ${isWord ? 'word' : 'char'} to password:`, randomChar);
-  //         selectedCharacters.push(randomChar);
-  //         password += randomChar;
-  //       }
-  //     }
-  //   } else {
-  //     console.log(`Skipping character. Exceeds password length.`);
-  //     const charSets = [];
+        if (randomChar.length + password.length <= passwordLength) {
+          console.log(`Adding ${isWord ? 'word' : 'char'} to password:`, randomChar);
+          selectedCharacters.push(randomChar);
+          password += randomChar;
+        }
+      }
+    } else {
+      console.log(`Skipping character. Exceeds password length.`);
+      const charSets = [];
   
-  //     if (useUpper && arr === upper) charSets.push(upper);
-  //     if (useLower && arr === lower) charSets.push(lower);
-  //     if (useNumbers && arr === numArr) charSets.push(numArr);
-  //     if (useSpecial && arr === special) charSets.push(special);
+      if (useUpper && arr === upper) charSets.push(upper);
+      if (useLower && arr === lower) charSets.push(lower);
+      if (useNumbers && arr === numArr) charSets.push(numArr);
+      if (useSpecial && arr === special) charSets.push(special);
   
-  //     while (password.length < passwordLength && charSets.length > 0) {
-  //       const randomSetIndex = Math.floor(Math.random() * charSets.length);
-  //       const randomSet = charSets[randomSetIndex];
-  //       let randomChar = getRandomCharacter(randomSet);
+      while (password.length < passwordLength && charSets.length > 0) {
+        const randomSetIndex = Math.floor(Math.random() * charSets.length);
+        const randomSet = charSets[randomSetIndex];
+        let randomChar = getRandomCharacter(randomSet);
   
-  //       if (randomChar.length + password.length <= passwordLength) {
-  //         console.log(`Adding char to password:`, randomChar);
-  //         selectedCharacters.push(randomChar);
-  //         password += randomChar;
-  //       } else {
-  //         charSets.splice(randomSetIndex, 1);
-  //       }
-  //     }
-  //   }
-  // }
+        if (randomChar.length + password.length <= passwordLength) {
+          console.log(`Adding char to password:`, randomChar);
+          selectedCharacters.push(randomChar);
+          password += randomChar;
+        } else {
+          charSets.splice(randomSetIndex, 1);
+        }
+      }
+    }
+  }
   
-  // if (useWords && useUpper) {
-  //   addCharacters(lowerWords, true);
-  //   addCharacters(upperWords, true);
-  // } else if (!useWords && useUpper) {
-  //   addCharacters(lower);
-  //   addCharacters(upper);
-  // } else if (useWords) {
-  //   addCharacters(lowerWords, true);
-  // } else {
-  //   addCharacters(lower);
-  // }
+  if (useWords && useUpper) {
+    addCharacters(lowerWords, true);
+    addCharacters(upperWords, true);
+  } else if (!useWords && useUpper) {
+    addCharacters(lower);
+    addCharacters(upper);
+  } else if (useWords) {
+    addCharacters(lowerWords, true);
+  } else {
+    addCharacters(lower);
+  }
 
   if (useSpecial) addCharacters(special, false);
 
@@ -168,7 +168,7 @@ const resetForm = () => {
   const form = document.getElementById("generatePassForm");
   form.reset();
 
-  // document.querySelector("#words").checked = false;
+  document.querySelector("#words").checked = false;
   document.querySelector("#special").checked = false;
   document.querySelector("#numbers").checked = false;
   document.querySelector("#upper").checked = false;
@@ -178,7 +178,7 @@ const resetForm = () => {
   generateBtn.textContent = "Generate Password";
   resetBtn.style.display = "none";
 
-  // isFirstClick = true;
+  isFirstClick = true;
 }
 
 const copyText = () => {
