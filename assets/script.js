682059389e4c11ae723d174f9345b68f9cdb37e6
Shanna-not-Shawna <144177634+Shanna-not-Shawna@users.noreply.github.com>
2024-06-22 08:15:51 -0500

    changed some const to lets

--- assets/script.js
+++ assets/script.js
@@ -5,12 +5,12 @@ const numArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 const lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 const upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-const passwordLength = 0;
-const useWords = false;
-const useSpecial = false;
-const useNumbers = false;
-const useUpper = false;''
-const useLower = false;
+let passwordLength = 0;
+let useWords = false;
+let useSpecial = false;
+let useNumbers = false;
+let useUpper = false;''
+let useLower = false;
 let isFirstClick = true;
 
 const generateBtn = document.querySelector("#generate");
